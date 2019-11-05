d13e76f0d28ca017dcfe243d34cb7e4c8ee93668
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2019-11-04 17:11:34 -0800

    add link to detail

--- src/components/Pokemon/PokemonList/PokemonList.js
+++ src/components/Pokemon/PokemonList/PokemonList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadListAction } from '../../../actions';
 
@@ -15,7 +16,7 @@ const PokemonList = () => {
   return (
     <div>
       <ul>
-        {pokemonList.map((pokemon, index) => (<li key={index}>{pokemon.name}</li>))}
+        {pokemonList.map((pokemon, index) => (<li key={index}><Link to={`/pokemons/${pokemon.name}`}>{pokemon.name}</Link></li>))}
       </ul>
       <button onClick={() => setOffset(offset + limit)}>Next</button>
       <button onClick={() => setOffset(offset - limit)}>Prev</button>
